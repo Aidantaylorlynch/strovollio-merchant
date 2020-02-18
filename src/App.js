133215b291a75f8819b0ac32af14541227c535ad
Aidan <aidan@Aidans-MacBook-Air.local>
2020-02-19 08:24:34 +1100

    blank

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import { OrderContainer } from './components/OrderContainer';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './styles/App.css';
-import { LoginContainer } from './components/loginContainer';
-import { OrderDetailsContainer } from './components/OrderDetailsContainer';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export class App extends Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<Switch>
-					<Route exact path="/">
-						<LoginContainer />
-					</Route>
-					<Route path="/orders">
-						<OrderContainer />
-					</Route>
-					<Route path="/orderdetails">
-						<OrderDetailsContainer />
-					</Route>
-				</Switch>
-			</BrowserRouter>
-		)
-	}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/store/actionsTypes.js
+++
@@ -1,4 +0,0 @@
-export const GET_ORDERS = "GET_ORDERS";
-export const SET_ORDERS = "SET_ORDERS";
-export const SET_MERCHANT = "SET_MERCHANT";
-export const SET_SELECTED_ORDER = "SET_SELECTED_ORDER";
